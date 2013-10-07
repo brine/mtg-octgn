5ece49acbc6ba967a3a5004114a746bacecb47bf
brine <spreetarded@gmail.com>
2013-10-07 03:12:44 -0400

    fixed the mana symbols showing up in the chat log

--- new_magic/scripts/changelog.py
+++ new_magic/scripts/changelog.py
@@ -3,5 +3,6 @@ changelog = {
         "Added a changelog dialog that displays when a game is started.",
         "Added a table submenu to separately enable/disable alignment, autoscripts, and attachment aligning.",
         "Attaching a card to another player's card will pass control of it to your opponent.",
-        "Manually moving a card around the table will move all its attachments with it (while attachment aligning is enabled)."])
+        "Manually moving a card around the table will move all its attachments with it (while attachment aligning is enabled).",
+        "Fixed the mana symbols showing up in the chat log."])
 }
