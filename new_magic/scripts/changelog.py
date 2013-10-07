1c0ee7eb457d3048b7fe21ed46b883a37de702f6
brine <spreetarded@gmail.com>
2013-10-07 13:00:34 -0400

    3.2.3.0 release pt 2 - updated changelog

--- new_magic/scripts/changelog.py
+++ new_magic/scripts/changelog.py
@@ -3,6 +3,13 @@ changelog = {
         "Added a changelog dialog that displays when a game is started.",
         "Added a table submenu to separately enable/disable alignment, autoscripts, and attachment aligning.",
         "Attaching a card to another player's card will pass control of it to your opponent.",
+        "Detached auras will trigger destroy scripts remotely for the player who controls the aura.",
         "Manually moving a card around the table will move all its attachments with it (while attachment aligning is enabled).",
-        "Fixed the mana symbols showing up in the chat log."])
+        "Fixed the mana symbols showing up in the chat log.",
+        "Added Theros Intro packs and Event deck.",
+        "RTR's Izzet Ingenuity intro deck now contains the correct basic lands.",
+        "Fixed Clear All (esc) not removing target arrows.",
+        "Clear All (esc) will not remove the Does Not Untap highlight.",
+        "Removed all manipulations of cards you don't control from alignment code (all done remotely now)."
+        ])
 }
