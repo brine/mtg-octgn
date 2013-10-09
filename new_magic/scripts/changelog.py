fe9530b6786a413ebe6018ea74246ba72da7676e
brine <spreetarded@gmail.com>
2013-10-09 02:37:05 -0400

    3.2.4.0 release

--- new_magic/scripts/changelog.py
+++ new_magic/scripts/changelog.py
@@ -11,5 +11,12 @@ changelog = {
         "Fixed Clear All (esc) not removing target arrows.",
         "Clear All (esc) will not remove the Does Not Untap highlight.",
         "Removed all manipulations of cards you don't control from alignment code (all done remotely now)."
+        ]),
+    3020400: ("3.2.4.0", "09 October 2013", [
+        "Fixed a game-breaking bug involving the attachments script.",
+        "Initial support for Monstrosity abilities.",
+        "Added marker images for Monstrous, Morph and Choice autoscript indicators.",
+        "Fixed activated model abilities asking to choose an ability twice.",
+        "Fixed a bug where casting an aura with a target would align the aura behind the target, instead of on the stack."
         ])
 }
