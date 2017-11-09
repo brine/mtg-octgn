98e7c70f86747ff0e569ea629821fe142a10aff1
brine <octgngamedirectory@gmail.com>
2017-11-08 23:43:26 -0500

    3.4.0.3 release

--- new_magic/scripts/changelog.py
+++ new_magic/scripts/changelog.py
@@ -224,7 +224,7 @@ changelog = {
         "Fixed issues with image downloader fetching some wrong token arts and not rotating plane cards.",
         "Please re-download the Tokens, Planechase, and Planechase 2012 images from the plugin to correct this error."
         ]),
-    3040002: ("3.4.0.2", "November 9 2017", [
+    3040003: ("3.4.0.3", "November 9 2017", [
         "Fixed many serious errors with the image downloader plugin.",
         "Added ability to download images from all sets at once",
         "Temporarily disabled Theros Challenge Decks and Unique Promotional Cards sets"
